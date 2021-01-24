3e4a83f1aabeb852e6607f629d0782ebd0e1c69e
”Dzmitry <dimonnn20@gmail.com>
2021-01-24 17:32:58 +0300

    Task 1

--- src/eu/senla/task1/Main.java
+++ src/eu/senla/task1/Main.java
@@ -1,11 +1,73 @@
 package eu.senla.task1;
 
 public class Main {
-    static int d = 112;
+
     public static void main (String[] args){
+        byte a1 = 12;
+        short b1 = 32767;
+        int c1 = 2147483647;
+        long d1 = 775808;
+        float e1 = 3.14856937f;
+        double f1 = 123.456;
+        char g1 = 'a';
+        boolean h1 = true;
+
+        Byte a2 = 3;
+        Short b2 = 500;
+        Integer c2 = 1000000;
+        Long d2 = 775808L;
+        Float e2 = 3.6377F;
+        Double f2 = 8174781D;
+        Character g2 = 'G';
+        Boolean h2  = false;
+
+        // Для примитивов
         Number number = new Number ();
-        number.setA((byte) d);
+        number.setA((byte) 127);
         System.out.println(number.getA());
 
+        number.setB((short) 12007);
+        System.out.println(number.getB());
+
+        number.setC((int) 61231231);
+        System.out.println(number.getC());
+
+        number.setD((long) 7367677867213l);
+        System.out.println(number.getD());
+
+        number.setE((float) 12.72867123f);
+        System.out.println(number.getE());
+
+        number.setF((double) 786348632346d);
+        System.out.println(number.getF());
+
+        number.setG((char) 'A');
+        System.out.println(number.getG());
+
+        number.setH((boolean) false);
+        System.out.println(number.getH());
+
+        // Для ссылок
+
+        number.setaByte((byte) 128);
+        System.out.println(number.getaByte()); // Слишком большое число для byte, выходит за пределы
+
+        number.setcInteger((int) 12.5);
+        System.out.println(number.getcInteger()); // Потеря точности при переходе с типа float в int
+
+        number.setdLong((long) 0.5);
+        System.out.println(number.getdLong()); // Потеря точности при переходе с типа float в int
+
+        number.seteFloat((float) 12.7762867123f);
+        System.out.println(number.geteFloat());
+
+        number.setfDouble((double) 12.7762867123);
+        System.out.println(number.getfDouble());
+
+        //number.setgCharacter((char) 'tata');  String не может быть приведен к char
+        //System.out.println(number.getgCharacter());
+
+        number.sethBoolean((boolean) true);
+        System.out.println(number.gethBoolean());
     }
 }
--- src/eu/senla/task1/Number.java
+++ src/eu/senla/task1/Number.java
@@ -1,20 +1,156 @@
 package eu.senla.task1;
 
 public class Number {
-    private byte a;
-    private Byte aByte;
 
+    byte a;
+    short b;
+    int c;
+    long d;
+    float e;
+    double f;
+    char g;
+    boolean h;
 
+    Byte aByte;
+    Short bShort;
+    Integer cInteger;
+    Long dLong;
+    Float eFloat;
+    Double fDouble;
+    Character gCharacter;
+    Boolean hBoolean;
+
+    // Для простых типов
     public byte getA() {
         return a;
     }
-    public void setA (byte a) {
-        this.a=a;
+
+    public void setA(final byte a) {
+        this.a = a;
     }
+
+    public short getB() {
+        return b;
+    }
+
+    public void setB(final short b) {
+        this.b = b;
+    }
+
+    public int getC() {
+        return c;
+    }
+
+    public void setC(final int c) {
+        this.c = c;
+    }
+
+    public long getD() {
+        return d;
+    }
+
+    public void setD (final long d) {
+        this.d = d;
+    }
+
+    public float getE() {
+        return e;
+    }
+
+    public void setE(final float e) {
+        this.e = e;
+    }
+
+    public double getF() {
+        return f;
+    }
+
+    public void setF(final double f) {
+        this.f = f;
+    }
+
+    public char getG() {
+        return g;
+    }
+
+    public void setG(final char g) {
+        this.g = g;
+    }
+
+    public boolean getH() {
+        return h;
+    }
+
+    public void setH(final boolean h) {
+        this.h = h;
+    }
+
+// Для ссылочных типов
+
     public Byte getaByte() {
         return aByte;
     }
-    public void setaByte (Byte aByte) {
-        this.aByte=aByte;
+
+    public void setaByte(final Byte aByte) {
+        this.aByte = aByte;
+    }
+
+    public Short getbShort() {
+        return bShort;
+    }
+
+    public void setbShort(final Short bShort) {
+        this.bShort = bShort;
+    }
+
+    public Integer getcInteger() {
+        return cInteger;
+    }
+
+    public void setcInteger(final Integer cInteger) {
+        this.cInteger = cInteger;
+    }
+
+    public Long getdLong() {
+        return dLong;
+    }
+
+    public void setdLong(final Long dLong) {
+        this.dLong = dLong;
+    }
+
+    public Float geteFloat() {
+        return eFloat;
+    }
+
+    public void seteFloat(final Float eFloat) {
+        this.eFloat = eFloat;
+    }
+
+    public Double getfDouble() {
+        return fDouble;
+    }
+
+    public void setfDouble(final Double fDouble) {
+        this.fDouble = fDouble;
+    }
+
+    public Character getgCharacter() {
+        return gCharacter;
+    }
+
+    public void setgCharacter(final Character gCharacter) {
+        this.gCharacter = gCharacter;
+    }
+
+    public Boolean gethBoolean() {
+        return hBoolean;
+    }
+
+    public void sethBoolean(final Boolean hBoolean) {
+        this.hBoolean = hBoolean;
     }
 }
+
+
+

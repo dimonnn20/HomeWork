703ef103fb7021636025188adb0cdc3f82f27da4
”Dzmitry <dimonnn20@gmail.com>
2021-01-24 12:18:49 +0300

    my first commit

---
+++ src/eu/senla/task1/Main.java
@@ -0,0 +1,11 @@
+package eu.senla.task1;
+
+public class Main {
+    static int d = 112;
+    public static void main (String[] args){
+        Number number = new Number ();
+        number.setA((byte) d);
+        System.out.println(number.getA());
+
+    }
+}
---
+++ src/eu/senla/task1/Number.java
@@ -0,0 +1,20 @@
+package eu.senla.task1;
+
+public class Number {
+    private byte a;
+    private Byte aByte;
+
+
+    public byte getA() {
+        return a;
+    }
+    public void setA (byte a) {
+        this.a=a;
+    }
+    public Byte getaByte() {
+        return aByte;
+    }
+    public void setaByte (Byte aByte) {
+        this.aByte=aByte;
+    }
+}
